14bf0bc62b781fc4f7f3883765ff66a75bf75662
lovetoken <lt.lovetoken@gmail.com>
2016-08-23 18:06:27 +0900

    readme.md adjust

--- R/efff.R
+++ R/efff.R
@@ -8,37 +8,28 @@
 #' data <- xts(round(index, 1), as.Date(16001:16020))
 #' names(data) <- paste("Fund", 1:3, sep="_")
 #' returns <- Xday_returns(data, 1)
+#'
+#' result <- efff(returns, rg = 0.012)
 
-efff <- function(returns, short = "no", max.allocation = NULL,
-                 risk.premium.up = .9, risk.increment = .0001){
+efff <- function(returns, short = "no", max.allocation = NULL, risk.premium.up = .9, risk.increment = .0001,
+                 rg = NA, plot = T){
 
   # pre
-  stopifnot(require(quadprog)); stopifnot(require(dplyr))
+  stopifnot(require(quadprog)); stopifnot(require(dplyr)); stopifnot(require(ggplot2))
 
-  # return argument should be a m x n matrix with one column per security
-  # short argument is whether short-selling is allowed; default is no (short
-  # selling prohibited) max.allocation is the maximum % allowed for any one
-  # security (reduces concentration) risk.premium.up is the upper limit of the
-  # risk premium modeled (see for loop below) and risk.increment is the
-  # increment (by) value used in the for loop
-
+  # content : origen code >> `eff.frontier()`
   covariance <- cov(returns)
-  print(covariance)
   n <- ncol(covariance)
 
-  # Create initial Amat and bvec assuming only equality constraint
-  # (short-selling is allowed, no allocation constraints)
   Amat <- matrix(1, nrow = n)
   bvec <- 1
   meq <- 1
 
-  # Then modify the Amat and bvec if short-selling is prohibited
   if(short == "no"){
     Amat <- cbind(1, diag(n))
     bvec <- c(bvec, rep(0, n))
   }
 
-  # And modify Amat and bvec if a max allocation (concentration) is specified
   if(!is.null(max.allocation)){
     if(max.allocation > 1 | max.allocation <0){
       stop("max.allocation must be greater than 0 and less than 1")
@@ -50,30 +41,58 @@ efff <- function(returns, short = "no", max.allocation = NULL,
     bvec <- c(bvec, rep(-max.allocation, n))
   }
 
-  # Calculate the number of loops
   loops <- risk.premium.up / risk.increment + 1
   loop <- 1
 
-  # Initialize a matrix to contain allocation and statistics
-  # This is not necessary, but speeds up processing and uses less memory
   eff <- matrix(nrow=loops, ncol=n+3)
 
-  # Now I need to give the matrix column names
   colnames(eff) <- c(colnames(returns), "Std.Dev", "Exp.Return", "sharpe")
 
-  # Loop through the quadratic program solver
   for (i in seq(from = 0, to = risk.premium.up, by = risk.increment)){
     dvec <- colMeans(returns) * i
 
-    # This moves the solution along the EF
     sol <- solve.QP(covariance, dvec = dvec, Amat = Amat, bvec = bvec, meq = meq)
-    eff[loop,"Std.Dev"] <- sqrt(sum(sol$solution*colSums((covariance*sol$solution))))
-    eff[loop,"Exp.Return"] <- as.numeric(sol$solution %*% colMeans(returns))
-    eff[loop,"sharpe"] <- eff[loop,"Exp.Return"] / eff[loop,"Std.Dev"]
-    eff[loop,1:n] <- sol$solution
+    eff[loop, "Std.Dev"] <- sqrt(sum(sol$solution*colSums((covariance*sol$solution))))
+    eff[loop, "Exp.Return"] <- as.numeric(sol$solution %*% colMeans(returns))
+    eff[loop, "sharpe"] <- eff[loop,"Exp.Return"] / eff[loop,"Std.Dev"]
+    eff[loop, 1:n] <- sol$solution
     loop <- loop+1
   }
 
+  pool <- as.data.frame(eff)
+
+  # condition rg
+
+  if(!is.na(rg)){
+
+    res <- pool[c(which.min(abs(pool$Exp.Return - rg)), which.max(pool$sharpe)), ]
+    res$Method <- c("Returns of goal Portfolio", "Market Portfolio")
+    res$Point_color <- c("green", "red")
+
+  } else {
+
+    res <- pool[which.max(pool$sharpe), ]
+    res$Method <- c("Market Portfolio")
+    res$Point_color <- c("red")
+
+  }
+
+  # ploting
+
+  if(plot){
+
+    P <- ggplot(pool, aes(x = Std.Dev, y = Exp.Return)) +
+      geom_point(size = .1, alpha = .01) +
+      geom_point(data = res[res$Method == "Returns of goal Portfolio", ], size = 2, color = "green") +
+      geom_point(data = res[res$Method == "Market Portfolio", ], size = 2, color = "red") +
+      theme_bw() + labs(title = "Efficient Frontier")
+
+    print(P)
+
+  }
+
   # return
-  eff %>% as.data.frame %>% return
+  attr(res, "poolset") <- pool %>% tbl_df
+  return(res)
+
 }

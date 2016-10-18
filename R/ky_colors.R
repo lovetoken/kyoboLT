9c5e2dfaa59904dbf8c7f3c33dd2d59bac6440ac
lovetoken <lt.lovetoken@gmail.com>
2016-10-18 17:20:08 +0900

    0.1.0.0094 version commit

--- R/ky_colors.R
+++ R/ky_colors.R
@@ -16,15 +16,17 @@ ky_colors <- function(n, alpha = 1){
 	# pre
 	n <- as.integer(n)
 
-	pal <- c(rgb(38 , 59 , 191, alpha * 255, maxColorValue = 255),
-	         rgb(81 , 179, 156, alpha * 255, maxColorValue = 255),
-	         rgb(151, 194, 61 , alpha * 255, maxColorValue = 255),
-	         rgb(158, 185, 218, alpha * 255, maxColorValue = 255),
-	         rgb(185, 225, 215, alpha * 255, maxColorValue = 255),
-	         rgb(213, 231, 177, alpha * 255, maxColorValue = 255),
-	         rgb(216, 227, 240, alpha * 255, maxColorValue = 255),
-	         rgb(191, 191, 191, alpha * 255, maxColorValue = 255),
-	         rgb(228, 228, 228, alpha * 255, maxColorValue = 255))
+	pal <- c(
+	  rgb(228, 228, 228, alpha * 255, maxColorValue = 255),
+	  rgb(216, 227, 240, alpha * 255, maxColorValue = 255),
+	  rgb(158, 185, 218, alpha * 255, maxColorValue = 255),
+	  rgb(185, 225, 215, alpha * 255, maxColorValue = 255),
+	  rgb(81 , 179, 156, alpha * 255, maxColorValue = 255),
+	  rgb(151, 194, 61 , alpha * 255, maxColorValue = 255),
+	  rgb(213, 231, 177, alpha * 255, maxColorValue = 255),
+	  rgb(38 , 59 , 191, alpha * 255, maxColorValue = 255),
+	  rgb(191, 191, 191, alpha * 255, maxColorValue = 255)
+	  )
 
 	# return
 	rep(pal, length = n)
